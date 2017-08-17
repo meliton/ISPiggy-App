4f1c62b1f822bb38436a7eccbebda8fc2e374a70
Mel <m_hinojosa@rocketmail.com>
2017-08-17 07:41:14 -0500

    Update ISPiggyActivity.java

--- ISPiggy/app/src/main/java/ispiggy/nothing/com/ispiggy/ISPiggyActivity.java
+++ ISPiggy/app/src/main/java/ispiggy/nothing/com/ispiggy/ISPiggyActivity.java
@@ -7,7 +7,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.SocketException;
+import java.text.MessageFormat;
 import java.util.Random;
+
 
 public class ISPiggyActivity extends AppCompatActivity {
 
@@ -24,6 +28,7 @@ public class ISPiggyActivity extends AppCompatActivity {
     TextView lblName;
     EditText txtDomain;
 
+    String sLblName = "Random Domain: "; // random domain label set
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +71,14 @@ public class ISPiggyActivity extends AppCompatActivity {
         RandomName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView tv = (TextView) findViewById(R.id.lblName);
-                tv.setText("RandomName was pressed");
+                //TextView tv = (TextView) findViewById(R.id.lblName);
+                //tv.setText("RandomName was pressed");
                 // EditText rv = (EditText)findViewById(R.id.txtDomain);
                 // rv.setText("RandomName");
-
+                String strWeb;
+                strWeb = makeDomainName();
+                lblName.setText(sLblName + strWeb);
+                txtDomain.setText(strWeb);
 
             }
         });
@@ -78,10 +86,15 @@ public class ISPiggyActivity extends AppCompatActivity {
         ToCom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView tv = (TextView) findViewById(R.id.lblName);
-                tv.setText("ToCom was pressed");
-                EditText rv = (EditText) findViewById(R.id.txtDomain);
-                rv.setText("ToCom");
+               // TextView tv = (TextView) findViewById(R.id.lblName);
+               // tv.setText("ToCom was pressed");
+               // EditText rv = (EditText) findViewById(R.id.txtDomain);
+               // rv.setText("ToCom");
+                String strDomain;
+                strDomain = String.valueOf(txtDomain.getText());
+                strDomain = String.valueOf(toDotCom(strDomain));
+                txtDomain.setText(strDomain);
+
             }
         });
 
@@ -98,10 +111,15 @@ public class ISPiggyActivity extends AppCompatActivity {
         MinusRand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView tv = (TextView) findViewById(R.id.lblName);
-                tv.setText("MinusRand was pressed");
-                EditText rv = (EditText) findViewById(R.id.txtDomain);
-                rv.setText("MinusRand");
+                //TextView tv = (TextView) findViewById(R.id.lblName);
+                //tv.setText("MinusRand was pressed");
+                //EditText rv = (EditText) findViewById(R.id.txtDomain);
+                //rv.setText("MinusRand");
+                String strDomain;
+                strDomain = String.valueOf(txtDomain.getText());
+                strDomain = String.valueOf(domainMinusRand(strDomain));
+                txtDomain.setText(strDomain);
+
             }
         });
 
@@ -118,19 +136,156 @@ public class ISPiggyActivity extends AppCompatActivity {
         MinusOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView tv = (TextView) findViewById(R.id.lblName);
-                tv.setText("MinusOne was pressed");
-                EditText rv = (EditText) findViewById(R.id.txtDomain);
-                rv.setText("MinusOne");
+                // TextView tv = (TextView) findViewById(R.id.lblName);
+                // tv.setText("MinusOne was pressed");
+                // EditText rv = (EditText) findViewById(R.id.txtDomain);
+                // rv.setText("MinusOne");
+                String strDomain;
+                strDomain = String.valueOf(txtDomain.getText());
+                strDomain = String.valueOf(domainMinusOne(strDomain));
+                txtDomain.setText(strDomain);
+
             }
         });
+}
+//////
+// Procedure : makeDomainName
+    public static String makeDomainName()
+    {
+        String sRandWeb;
+        int iDomain;
+        int x;
+
+        String[] astrVowels;
+        String[] astrConsonants;
+        String[] astrTLDsuffix;
+
+        astrVowels = new String[] {"a", "e", "i", "o", "u", "y"};
+        astrConsonants = new String[] {"c", "d", "f", "h", "l", "m", "n", "r", "s", "t"};
+        astrTLDsuffix = new String[] {".com", ".com", ".net", ".org"};
+
+        sRandWeb = "";
+        x = 0;
+
+        int iMinDomain = 5; // minimum word size for domain name
+        int iMaxDomain = 8; // maximum word size for domain name
+
+        while ((x < (Integer) myRandom(iMinDomain, iMaxDomain))) {
+            if (((Integer) isOdd(x) == 1)) {
+                //  even number generates random vowel
+                sRandWeb = (sRandWeb + astrVowels[(Integer) myRandom(0, (astrVowels.length))]);
+            }
+            else {
+                //  odd number generates random consonant
+                sRandWeb = (sRandWeb + astrConsonants[(Integer) myRandom(0, (astrConsonants.length))]);
+            }
+
+            x = (x + 1);
+        }
+
+        iDomain = (Integer) myRandom(0, (astrTLDsuffix.length - 1));
+        //  get random domain suffix
+        return (sRandWeb + astrTLDsuffix[iDomain]);
+    }
+
+    // Procedure : myRandom
+    public static Object myRandom(int iMinVal, int iMaxVal)
+    {
+        return (int)Math.floor((double) (((iMaxVal - iMinVal) + 1) * Math.random()) + iMinVal);
+    }
+
+    // Procedure : isOdd
+    public static Object isOdd(int iNumber)
+    {
+        Object tempisOdd;
+        if ((int)Math.floor(iNumber) % 2 > 0)
+        {
+            tempisOdd = 0; // Odd Number, return 0
+        }
+        else
+        {
+            tempisOdd = 1; // Even Number, return 1
+        }
+        return tempisOdd;
+    }
+
+    // Procedure : toDotCom
+    public static Object toDotCom(String strToCom)
+    {
+        int iLength;
+        int iDotLoc;
+        int iDomOnly;
+
+        iLength = strToCom == null ? 0 : strToCom.length(); // get overall domain length
+        iDotLoc = strToCom.indexOf(".") + 1; // get dot location in domain
+        iDomOnly = (iLength - (iLength - iDotLoc + 1)); // get size of domain
+
+        if (iDomOnly >= 2) // domain has at least two chars
+        {
+            // save only domain into variable and add .com
+            strToCom = strToCom.substring(0, iDomOnly) + ".com";
+        }
+        return strToCom;
+    }
+
+    // Procedure : domainMinusRand
+    public static Object domainMinusRand(String strDomain)
+    {
+        Object tempdomainMinusRand = null;  // remove null once this object is fixed below
+        int iLength;
+        int iDotLoc;
+        int iDomOnly;
+        int iRndChar;
+        String sExtOnly;
+        String sRndChar;
+
+        iLength = strDomain == null ? 0 : strDomain.length(); // get overall domain length
+        iDotLoc = strDomain.indexOf(".") + 1; // get dot location in domain
+        iDomOnly = (iLength - (iLength - iDotLoc + 1)); // get size of domain
+
+        if (iDomOnly >= 3) // domain has at least two chars
+        {
+            // save extension into variable
+            sExtOnly = strDomain.substring(iDotLoc - 1, iDotLoc - 1 + iLength - iDotLoc + 1);
+            strDomain = strDomain.substring(0, iDomOnly); // save only domain into variable
+
+            iRndChar = (Integer) myRandom(1, iDomOnly); // get random char int location from domain length
+            sRndChar = strDomain.substring(iRndChar - 1, iRndChar - 1 + 1); // gets random char from domain
+
+            // remove char from domain and reassemble domain name
+            //FIX THIS PART OF THE CODE---------------------------------------------------------------------------
+            //tempdomainMinusRand = Microsoft.VisualBasic.Strings.Replace(strDomain, sRndChar, "",, 1) + sExtOnly;
+            //----------------------------------------------------------------------------------------------------
+            strDomain = (String) tempdomainMinusRand; // puts new domain in passed domain variable
+        }
+        return strDomain;
+    }
+
+    // Procedure : domainMinusOne
+    public static Object domainMinusOne(String strDomain)
+    {
+        Object tempdomainMinusOne;
+        int iLength;
+        int iDotLoc;
+        int iDomOnly;
+        String sExtOnly;
+
+        iLength = strDomain == null ? 0 : strDomain.length(); // get overall domain length
+        iDotLoc = strDomain.indexOf(".") + 1; // get dot location in domain
+        iDomOnly = (iLength - (iLength - iDotLoc + 1)); // get size of domain
+
+        if (iDomOnly >= 3) // domain has at least two chars
+        {
+            // save extension into variable
+            sExtOnly = strDomain.substring(iDotLoc - 1, iDotLoc - 1 + iLength - iDotLoc + 1);
+            strDomain = strDomain.substring(0, iDomOnly); // save only domain into variable
+
+            // remove last char in domain name and reassemble domain name
+            tempdomainMinusOne = strDomain.substring(0, iDotLoc - 2) + sExtOnly;
+            strDomain = (String) tempdomainMinusOne; // puts new domain in passed domain variable
+        }
+        return strDomain;
+    }
 
 
-
-
-
-
-
-    }}
-
-
+}
